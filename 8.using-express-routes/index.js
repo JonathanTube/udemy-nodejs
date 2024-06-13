ab3ad38f1c2b5cf3925f1e0f2fd10ba48ac7a852
Jonathan <275568635@qq.com>
2024-06-13 17:01:32 +0800

    fix bug.

--- 8.using-express-routes/index.js
+++ 8.using-express-routes/index.js
@@ -10,11 +10,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(formRouter)
 // I should put userRouter at the bottom, becase '/' route exists in in user router.
 // otherwise all of the requests will be intercepted.
-app.use(userRouter)
+
+// we can also add a prefix at the beginning of the path, then only matches /user-api can be routed to user router.
+app.use("/user-api", userRouter)
 
 // add a default middleware to intercept 404 requests
 app.use((req, res, next) => {
   // status, not statusCode!
   res.status(404).send("page not found")
 })
-app.listen(3000)
+app.listen(4000)
